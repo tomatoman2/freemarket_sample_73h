b2b36381715b7c92b19f12d49139cf42048927ec
norio-tech <fukagawa0420@gmail.com>
2020-04-24 11:56:20 +0900

    product,product_imageのマイグレーション及びモデルの追記

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :postage_code, presence: true
+  validates :explanation, presence: true
+  validates :status, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_time_code, presence: true
+
+  has_many :likes
+  has_many :comments
+  has_many :product_images
+  belongs_to :order
+  belongs_to :user
+  belongs_to :brand
+  belongs_to :category
+  belongs_to :code
 end

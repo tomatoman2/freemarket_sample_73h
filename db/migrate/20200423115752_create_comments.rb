6c04e63a8758f86402ef135d6e39e3d34a9423fa
miyamiya37 <sphr3cu9@mountain.ocn.ne.jp>
2020-04-25 12:30:16 +0900

    like&comment訂正

--- db/migrate/20200423115752_create_comments.rb
+++ db/migrate/20200423115752_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.references :user_id ,foreign_key: true
-      t.references :product_id ,foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :product, foreign_key: true
       t.string :message
       t.timestamps
     end

0f5176ff73cf643efa3e3dd78e8ac4fa102be112
norio-tech <fukagawa0420@gmail.com>
2020-04-27 22:35:06 +0900

    商品出品ページ（マークアップ）の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
+  GROUP_ITEM_STATUS = 100
+  GROUP_ITEM_POSTAGE = 101
+  GROUP_ITEM_DELIVERY_TIME = 102
   def index
+  end
+  def new
+
+    @item_explanation_placeholder = "商品の説明（必須 1,000文字以内）\r\n（色、素材、重さ、定価、注意点など）\r\n\r\n例）2010年頃に1万円で購入したジャケットです。ライトグレーで傷はありません。あわせやすいのでおすすめです。"
+    @item = Product.new
+    @item.product_images.build
+    @statuses = Code.group_search(GROUP_ITEM_STATUS)
+    @postages = Code.group_search(GROUP_ITEM_POSTAGE)
+    @delivery_times = Code.group_search(GROUP_ITEM_DELIVERY_TIME)
+    @prefectures = Prefecture.all
+
+
+  end
+  def create
+
   end
 end

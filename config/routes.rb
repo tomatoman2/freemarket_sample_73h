de3f7cff32c5298b292d2368a7c7a980e1efe44c
1889yasuto <3351vasuto@gmail.com>
2020-04-29 21:14:35 +0900

    ユーザーマイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   }
 
   root 'items#index'
+  resource  :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

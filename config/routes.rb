ff39fd018ee6a515eabdf2936543f4acaf2c8076
1889yasuto <3351vasuto@gmail.com>
2020-04-30 12:53:45 +0900

    マイページ修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   }
 
   root 'items#index'
-  resource  :users
+  resources  :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

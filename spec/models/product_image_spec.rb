dce30e6f8e119a0525293583ad468de6f9992e1e
norio-tech <fukagawa0420@gmail.com>
2020-05-06 16:55:25 +0900

    rspecを使用した単体テストファイルを作成

---
+++ spec/models/product_image_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+describe ProductImage do
+  describe '#create' do
+    context 'can save' do
+      it 'imageがあれば保存できること' do
+        expect(FactoryBot.build(:product_image)).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it 'invalid name' do
+        productimage = FactoryBot.build(:product_image, image_name: nil)
+        productimage.valid?
+        expect(productimage.errors[:image_name]).to include("を入力してください")
+      end
+    end
+  end
+end

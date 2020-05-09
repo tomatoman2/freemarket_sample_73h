896f33924947f5ba96ac7c39eb0464b9db1fd329
tomatoman2 <kasuga01m@gmail.com>
2020-05-09 19:16:31 +0900

    Fix:本番環境画像保存の設定

--- Gemfile
+++ Gemfile
@@ -80,6 +80,8 @@ gem 'pry-rails'
 gem 'carrierwave'
 gem 'mini_magick'
 gem "jquery-rails"
+gem 'fog-aws'
+
 group :production do
   gem 'unicorn'
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,7 +14,7 @@ class Product < ApplicationRecord
 
   has_many :product_images, dependent: :destroy
   #テスト段階ではとりあえず全てにoptional:trueをつけておく。関連テーブルのデータがなくても登録できるようにする
-  has one :order, optional: true
+  has_one :order
   belongs_to :user, optional: true
   belongs_to :brand, optional: true
   belongs_to :category, optional: true

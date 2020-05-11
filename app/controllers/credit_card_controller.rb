b44e8fb3ff20f8069279c3896a9064ad44b35370
tanakaka773 <yamadataro881@gmail.com>
2020-05-11 23:41:46 +0900

    payjp機能、購入機能追加

---
+++ app/controllers/credit_card_controller.rb
@@ -0,0 +1,81 @@
+class CreditCardController < ApplicationController
+
+  require "payjp"
+
+  before_action :set_item, only: [:pay, :new, :show]
+
+  def new
+    begin
+      card = CreditCard.where(user_id: current_user.id)
+      redirect_to action: "show" if card.exists?
+    rescue => exception
+      redirect_to root_path
+    end
+  end
+
+  def pay #payjpとCardのデータベース作成を実施します
+    begin
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      if params['payjp-token'].blank?
+        redirect_to action: "new"
+      else
+        customer = Payjp::Customer.create(
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
+        ) 
+        @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+        if @card.save
+          if params[:product_id]
+            redirect_to new_order_path(id: params[:product_id])
+          else 
+            redirect_to user_path(current_user.id)
+          end
+        end
+      end
+    rescue => exception
+      @error_messages = "登録できませんでした"
+      render :new 
+    end
+  end
+
+  def delete #PayjpとCardデータベースを削除します
+    begin
+      card = CreditCard.where(user_id: current_user.id).first
+      if card.blank?
+      else
+        Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+        customer = Payjp::Customer.retrieve(card.customer_id)
+        customer.delete
+        card.delete
+      end
+        redirect_to action: "new"
+    rescue => exception
+      @error_messages = "削除できませんでした"
+      set_show_default_value
+      render :show
+    end
+  end
+
+  def show #Cardのデータpayjpに送り情報を取り出します
+    set_show_default_value
+  end
+
+  private
+  def set_item
+    @error_messages = ""
+  end
+
+  def set_show_default_value #Cardのデータpayjpに送り情報を取り出します
+    card = CreditCard.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+end
+
+
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,75 @@
 class OrdersController < ApplicationController
+  GROUP_ITEM_POSTAGE = 100
+  GROUP_ITEM_POSTAGE = 101
+  GROUP_ITEM_DELIVERY_TIME = 102
+
+  before_action :set_item, only: [:new, :create]
+
 
   def new
+    begin
+      default_card_information
+    rescue => exception
+      @error_messages = "例外処理が発生しました"
+      redirect_to root_path
+    end
   end
   
+  def create
+    begin
+      @order = Order.new(order_params)
+      if @order.save
+        card = CreditCard.where(user_id: current_user.id).first
+        Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+        Payjp::Charge.create(
+          amount: @order.product.price,
+          customer: card.customer_id, 
+          currency: 'jpy' 
+        )
+        redirect_to index_path 
+      else
+        render :new
+      end
+    rescue => exception
+      @error_messages = "購入できませんでした"
+      params[:id] = order_params[:product_id]
+      default_card_information
+      render :new
+    end
+  end
+
+
+  private 
+  def order_params
+    params.require(:order).permit(:product_id)
+      .merge(user_id:current_user.id)
+  end
+
+  def set_item
+    @error_messages = ""
+  end
+
+  def default_card_information
+    @product = Product.find(params[:id])
+    @image = ProductImage.find_by(product_id: params[:id])
+    @postage = Code.where(group_id: GROUP_ITEM_POSTAGE, code_id:@product.postage_code)
+    @address = Address.find_by(user_id: current_user.id)
+    @prefecture = Prefecture.find(@address.prefecture_id)
+    @order = Order.new
+    #CreditCardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    card = CreditCard.where(user_id: current_user.id).first
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to new_credit_card_path(product_id:@product.id)
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
 end
+
+
+
